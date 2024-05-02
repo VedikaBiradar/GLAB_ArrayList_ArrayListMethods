c3fb25cc4b627061475ac67fdac9659dcf642dd5
VedikaBiradar <vedikabiradar2001@gmail.com>
2024-05-02 17:42:33 -0400

    Initial commit

---
+++ src/com/vedika/ArrayListAndArrayListMethods/Main.java
@@ -0,0 +1,26 @@
+package com.vedika.ArrayListAndArrayListMethods;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        createArrayListExample ca=new createArrayListExample();
+//        ca.createArrayList();
+
+//        CreateArrayListFromAnotherCollection ca=new CreateArrayListFromAnotherCollection();
+//        ca.arrayListFromAnthorCollection();
+
+//        EditingArrayListElements ea = new EditingArrayListElements();
+//        ea.editArrayListElements();
+
+//         RemoveElementsFromArrayList re=new RemoveElementsFromArrayList();
+//         re.removeElementsFromArrayList();
+
+//           IterativeThroughArrayList it=new IterativeThroughArrayList();
+//           it.iterativethroughArrayList();
+
+           SearchingElement se=new SearchingElement();
+           se.searchelement();
+
+
+    }
+}
